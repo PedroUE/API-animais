ef59c4745fb92506715f68d46341cf51aa2676b1
Pedro Escapalete <pedro.escapalete@aluno.senai.br>
2025-09-22 14:01:25 -0300

    finalizao create

--- src/controller/animaisControllers.js
+++ src/controller/animaisControllers.js
@@ -11,7 +11,53 @@ const getAllAnimais = (req, res) => {
     });
 }
 
+const getAnimaisById = (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const animal = animais.find(a => a.id === id);
+
+    if (!animal) {
+        res.status(404).json({
+            success: false,
+            message: `O animal não existe! `
+        });
+    }
+
+    res.status(200).json({
+        total: animal.length,
+        animal: animal
+    });
+}
+
+const creatAnimal = (req, res) => {
+    const {nome, especie, raca, idade, peso, propietário, vacinado} = req.body;
+    
+    if(!nome || !especie || !raca || !idade || !peso) {
+        return res.status(400).json({
+            success: false,
+            message: "Nome, especie, raca, idade e peso são obrigatórios para criar um animal!"
+        });
+    }
+
+    const novoAnimal = {
+        id: animais.length + 1,
+        nome,
+        especie,
+        raca,
+        idade,
+        peso,
+        propietário,
+        vacinado
+    }
+
+    animais.push(novoAnimal);
+
+    res.status(201).json({
+        sucess: true,
+        message: "animal criado com sucesso!",
+        animal: novoAnimal
+    });
+}
 
 
-
-export {getAllAnimais};
+export {getAllAnimais, getAnimaisById};
