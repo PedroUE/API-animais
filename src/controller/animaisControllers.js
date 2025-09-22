a84854be05cd5db45f6d1cdc26994a1f03a5d2d4
Pedro Escapalete <pedro.escapalete@aluno.senai.br>
2025-09-22 14:17:19 -0300

    finaliza o delete

--- src/controller/animaisControllers.js
+++ src/controller/animaisControllers.js
@@ -68,7 +68,55 @@ const deleteAnimal = (req, res) => {
             message:`O animal com o id: ${id} não existe!`
         });
     }
+
+    const animalParaRemover = animais.find(a => a.id === id);
+
+    if(!animalParaRemover) {
+        return res.status(404).json({
+            seccess: false,
+            message: `O animal com o id: ${id} não existe!`
+        });
+    }
+
+    const animalFilter = animais.find(a => a.id !== id);
+
+    animais.splice(0, animais.length, ...animalFilter);
+
+    res.status(200).json ({
+        sucess: true,
+        message: `O animal de id: ${id} foi removido com sucesso! `
+    });
 }
 
+
+
+const updateAnimais = (req, res) => {
+    const id = parseInt(req.params.id);
+    const {nome, especie, raca, idade, peso, propietário, vacinado} = req.body
+    const idParaEditar = id;
+
+    if (isNaN(idParaEditar)) {
+        return res.status(400).json({
+            sucess: false,
+            message: `O ID deve ser válido`
+        });
+    }
+
+    const animalExistente = animais.find(animal => animal.id === idParaEditar);
+
+    if(!animalExistente) {
+        return res.status(404).json({
+            sucess: false,
+            message: `Nenhum animal com o id: ${id} não foi encontrado`
+        });
+    }
+
+    const animaisAtualizados = animais.map(animal => animal.id === idParaEditar ? {
+        ...animal,
+        ...(nome && {nome}),
+    }
+
+    );
+}
 
 export {getAllAnimais, getAnimaisById, createAnimal};
